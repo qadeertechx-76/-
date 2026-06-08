ab6bf35ddb5dcfdaf08ef167dc5544f00fb2698a
qadeertechx-76 <qadeertechx@gmail.com>
2026-06-08 18:06:28 +0500

    Update tool-voice.js

--- plugins/tool-voice.js
+++ plugins/tool-voice.js
@@ -73,7 +73,7 @@ async (conn, mek, m, {
 
         // Send menu message with image
         const sentMsg = await conn.sendMessage(from, {  
-            image: { url: "https://files.catbox.moe/7zfdcq.jpg" },
+            image: { url: "https://eliteprotech-url.zone.id/1780825791553u2mng8.jpg" },
             caption: menuText
         }, { quoted: m });
 
